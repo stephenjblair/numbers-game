d6a70e8e78952d1702c872b39a195090b4ee5f90
Stephen Blair <stephenjohnblair@outlook.com>
2020-04-06 16:36:42 +0100

    Added dynamic behaviour to new child

---
+++ src/QuizOptions.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class QuizOptions extends Component {
+    render() {
+        return (
+            <div className="fields">
+                <div className="field-block">{this.props.option}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default QuizOptions;
